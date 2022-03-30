a1cd26a042b5eddee008376c2141f6a7ee106a61
Rachael Schlosberg <rachael.schlosberg@gmail.com>
2022-03-30 11:32:48 -0500

    Feature 14 complete

--- projects/views.py
+++ projects/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView
 from django.views.generic.detail import DetailView
@@ -21,3 +22,20 @@ class ProjectDetailView(LoginRequiredMixin, DetailView):
 
     def get_queryset(self):
         return Project.objects.filter(members=self.request.user)
+
+
+class ProjectCreateView(LoginRequiredMixin, CreateView):
+    model = Project
+    template_name = "projects/create.html"
+    fields = ["name", "description", "members"]
+    success_url = reverse_lazy("show_project")
+
+    # very unsure about part below. Wasn't sure how to
+    # assign item.members = self.request.user
+
+    def form_valid(self, form):
+        item = form.save(commit=False)
+        item.auth_User = self.request.user
+        item.save()
+        form.save_m2m()
+        return redirect("show_project", pk=item.id)
